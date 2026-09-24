da996ee98df89da00fdc35d77697770663fa575b
Abhinav_K <k59412736@gmail.com>
2026-09-24 14:06:30 +0530

    test agent

--- order_service/src/order.controller.ts
+++ order_service/src/order.controller.ts
@@ -8,11 +8,11 @@ export class OrderController {
 
   @MessagePattern('create_order')
   createOrder(@Payload() Payload: {userId: number;productId:number,quantity:number}){
-    return this.orderService.createOrder(Payload.userId,Payload.productId,Payload.quantity)
+    return this.orderService.createOrder(Payload.userId,Payload.productId,Payload.quantity);
   }
 
   @MessagePattern('get_orders')
   getOrders(@Payload() Payload:{userId:number}){
-    return this.orderService.getOrderHistory(Payload.userId)
+    return this.orderService.getOrderHistory(Payload.userId);
   }
 }
